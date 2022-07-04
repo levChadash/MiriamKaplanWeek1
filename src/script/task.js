fa91584d756a9c4868ba3bdeb722498a8cc54b91
מרים <miriamkaplan123@gmail.com>
2022-07-04 17:52:28 +0300

    promise2

--- src/script/task.js
+++ src/script/task.js
@@ -5,5 +5,17 @@
   myPromise.then(function() {
     return Math.random()*10;
   });
+   function setUpper(array){
+    const upper = arr.map(element => {
+        return element.toUpperCase();
+      })
+      return upper;
+   }
   
-  
+  function myFunction(array) {
+      
+    return Promise.resolve(setUpper(array));
+  }
+  myFunction.then(
+    function(value){return value.sort();}
+  );
